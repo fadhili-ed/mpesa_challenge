949211fac977dd754ed40c81315957cd8e4ed234
fadhili-ed <fadhilied00@gmail.com>
2020-02-14 06:50:44 +0300

    withdraw

--- lib/account.rb
+++ lib/account.rb
@@ -1,11 +1,26 @@
 class Account 
-    attr_accessor :balance  
+    attr_accessor :balance, :pin
+    @@AGENT_NUMBERS = [111111, 222222, 333333, 44444] 
     def initialize
         @balance = 0 
+        @pin = 1234
     end
-
+    def self.agent_numbers
+        @@AGENT_NUMBERS 
+    end
     def check_balance 
         "Your current balance is #{@balance}"
+    end
+    def withdraw(agent_number, amount, pin)
+        return 'invalid agent number try again' unless @@AGENT_NUMBERS.include?(agent_number)
+        return 'invalid pin number try again' unless @pin == pin
+        if amount > @balance
+            "Insufficient balance,you cannot withdraw #{amount},your current balance is #{@balance}"
+        else
+            @balance -=amount
+
+            "Confirmed you withdrew #{amount}, your new balance #{balance} at #{current_time()}"
+        end
     end
     def deposit (amount)
         @balance += amount       
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -8,6 +8,34 @@ describe Account do
         balance = subject.balance
         expect(subject.check_balance).to eq "Your current balance is #{balance}"
     end
+    it 'returns reduces balance on withdrawal' do
+        agent_number = Account.agent_numbers.first        
+        subject.deposit(1000)
+        subject.withdraw(agent_number, 200, 1234 )        
+        expect(subject.check_balance).to eq "Your current balance is 800"
+
+    end
+    it 'returns message on withdrawal' do
+        agent_number = Account.agent_numbers.first
+        pin = subject.pin
+        subject.deposit(1000)     
+        expect(subject.withdraw(agent_number, 200, pin)).to eq "Confirmed you withdrew 200, your new balance 800 at #{subject.current_time()}"
+    end
+    it  'returns error if agent number is invalid' do
+        subject.deposit(1000) 
+        expect(subject.withdraw(000, 200, 1234 )).to eq "invalid agent number try again"
+    end
+    it  'returns error if pin is invalid' do
+        agent_number = Account.agent_numbers.first
+        subject.deposit(1000) 
+        expect(subject.withdraw(agent_number, 200, 12)).to eq "invalid pin number try again"
+    end
+    it 'returns error if current balance less than withdrawn amount' do
+        agent_number = Account.agent_numbers.first
+        pin = subject.pin
+        balance = subject.balance
+        expect(subject.withdraw(agent_number, 200, pin)).to eq "Insufficient balance,you cannot withdraw 200,your current balance is #{balance}"
+    end
     it 'returns  increase balance on deposit' do
         subject.deposit(500)
         expect(subject.balance).to eq 500
